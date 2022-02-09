be3a94b98f29982ced4d23fdb373441844995656
BearForesterSmallBeef <BearForest2006@gmail.com>
2022-02-09 17:47:58 +0300

    1

--- main.py
+++ main.py
@@ -3,58 +3,76 @@ from flask import Flask, url_for
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/index')
+@app.route("/")
+def main_menu():
+    return \
+        f'''
+        <h1>Миссия Колонизация Марса</h1>
+        <ol>
+        <li><a href="http:\\promotion">Promotion</a>
+        <li><a href="http:\\index">Index</a>
+        <li><a href="http:\\image_mars">Image of mars</a> 
+        <li><a href="http:\\promotion_image">Promotion with image</a> 
+        </ol>
+        '''
+
+
+@app.route("/index")
 def index():
-    return "Привет, Яндекс!"
+    return f"""<h1>И на Марсе будут яблони цвести!</h1>"""
 
 
-@app.route('/countdown')
-def countdown():
-    countdown_list = [str(i) for i in range(10, 0, -1)]
-    countdown_list.append("Пуск")
-    return '<br>'.join(countdown_list)
+@app.route("/promotion")
+def promotion():
+    return \
+        f"""
+        <div>Человечество вырастает из детства.<br>
+        Человечеству мала одна планета.<br>
+        Мы сделаем обитаемыми безжизненные пока планеты.<br>
+        И начнем с Марса!<br>
+        Присоединяйся!</div>"""
 
 
-@app.route('/image_sample')
-def image():
-    return f"""<img src="{url_for('static', filename='/img/webserver-1-7.jpeg')}"
-alt="здесь должна была быть картинка, но она не нашлась!">"""
+@app.route("/image_mars")
+def image_mars():
+    return \
+        f'''
+        <title>Привет, Марс!</title>
+        <h1>Жди нас, Марс!</h1>
+        <img src={url_for('static', filename='/img/Mars.jpg')} alt="Марс сломался :(" align="middle" 
+        height=350 width=500>
+        <div>Вот она какая, красная планета</div>
+        '''
 
 
-@app.route('/sample_page')
-def sample_page():
-    return f"""<!DOCTYPE html>
-            <html lang="ru">
-            <head>
-                <meta charset="UTF-8">
-                <link rel="stylesheet" type="text/css"
-                    href="{url_for('static', filename='css/style.css')}">
-                <title>Привет, Яндекс!</title>
-            </head>
-            <body>
-            <h1>Первая страница</h1>
-            </body>
-            </html>"""
-
-
-@app.route('/bootstramp_sample')
-def bootstramp_sample():
-    return f"""<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Hello, world!</title>
-</head>
-<body>
-<h1>Hello, world!</h1>
-<div class="alert alert-primary" role="alert">
-    А мы тут компонентами Bootstrap балуемся
-</div>
-</body>
-</html>"""
+@app.route("/promotion_image")
+def promotion_image():
+    return \
+        f'''
+        <!DOCTYPE html>
+        <html lang="ru">
+        <head>
+            <meta charset="UTF-8">
+            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='/css/style.css')}">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
+            crossorigin="anonymous">
+            <link rel="stylesheet">
+            <title>Привет, Марс!</title>
+        </head>
+        <body>
+            <h1>Жди нас, Марс!</h1>
+            <img src={url_for('static', filename='/img/Mars.jpg')} alt="Марс сломался :(" align="middle" 
+            height=350 width=500>
+            <div class="alert alert-light" role="alert">Вот она какая, красная планета</div>
+            <div class="alert alert-info" role="alert">Человечество вырастает из детства.<br>
+            Человечеству мала одна планета.<br>
+            Мы сделаем обитаемыми безжизненные пока планеты.<br>
+            И начнем с Марса!<br>
+            Присоединяйся!</div>
+        </body>
+        </html>
+        '''
 
 
 if __name__ == '__main__':
